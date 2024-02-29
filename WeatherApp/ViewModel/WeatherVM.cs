3623ddda19133f1f33a81f2f7beef6186b8a4618
Taeguk Seok <taeguk.seok@gmail.com>
2024-02-29 15:21:11 -0500

    Finished main setup for the MVVM App.

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string querry;
+
+        public string Querry
+        {
+            get { return querry; }
+            set 
+            { 
+                querry = value;
+                OnPropertyChanged ("Querry)");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
